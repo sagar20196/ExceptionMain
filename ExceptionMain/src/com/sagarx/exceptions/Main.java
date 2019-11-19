61ac2aaa8be5a2d48834a09d0dfc0a9f0d15ef62
sagarr20196 <57824451+sagarr20196@users.noreply.github.com>
2019-11-19 11:57:17 +0530

    Hello World

---
+++ ExceptionMain/src/com/sagarx/exceptions/Main.java
@@ -0,0 +1,13 @@
+package com.sagarx.exceptions;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
