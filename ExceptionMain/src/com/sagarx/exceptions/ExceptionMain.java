85e4e6b560d220d3bed87268bebeae9b41608807
sagarr20196 <57824451+sagarr20196@users.noreply.github.com>
2019-11-19 11:39:30 +0530

    First Commit

--- ExceptionMain/src/com/sagarx/exceptions/ExceptionMain.java
+++ ExceptionMain/src/com/sagarx/exceptions/ExceptionMain.java
@@ -1,5 +1,15 @@
 package com.sagarx.exceptions;
 
+/**
+ * Throwable
+ * 			Error- We can't do anything because a reasonable applications should not try to catch. All the subclasses of error should not try to
+ * 					handle it. Those situations are beyond our control.
+ * 			Exception- This condition is under control because a reasonable applications might try to catch.
+ * 						Checked Exception- Checked Exceptions are exception scenario that we can anticipate in a program and try to recover from it. 
+ * 						Runtime Exception- These are caused by bad programming. Arithmetic exception is Runtime Exception.
+ * 
+ *
+ */
 public class ExceptionMain {
 
 	public ExceptionMain() {
